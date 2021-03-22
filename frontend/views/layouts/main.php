d7fdc608bc0cd7e53ba3adb43043f24785111599
Михаил <serov.m@yandex.ru>
2021-03-23 04:59:00 +0500

    _

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -23,8 +23,8 @@ AppAsset::register($this);
     
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css"> -->
 </head>
 <body>
 <?php $this->beginBody() ?>
@@ -126,7 +126,7 @@ AppAsset::register($this);
       </div>
       <div class="header__account">
         <a class="header__account-photo">
-          <img src="./img/user-photo.png"
+          <img src="../img/user-photo.png"
                width="43" height="44"
                alt="Аватар пользователя">
         </a>
@@ -193,7 +193,7 @@ AppAsset::register($this);
       <div class="page-footer__copyright">
         <a>
           <img class="copyright-logo"
-               src="./img/academy-logo.png"
+               src="../img/academy-logo.png"
                width="185" height="63"
                alt="Логотип HTML Academy">
         </a>
