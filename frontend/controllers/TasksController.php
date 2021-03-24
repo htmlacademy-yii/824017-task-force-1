4b5b2336f630a284997b5ec4285a550702a7aa56
mikeserov <serov.m@yandex.ru>
2021-03-25 04:04:37 +0500

    Переносит логику запроса из контроллера в модель

--- frontend/controllers/TasksController.php
+++ frontend/controllers/TasksController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace frontend\controllers;
 
-use TaskForce\Controllers\Task;
 use yii\web\Controller;
 use frontend\models\Tasks;
 
@@ -12,7 +11,7 @@ class TasksController extends Controller
 {
     public function actionIndex(): string
     {
-        $tasks = Tasks::find()->with('specialization')->where(['status' => Task::STATUS_NEW])->orderBy(['posting_date' => SORT_DESC])->asArray()->all();
+        $tasks = Tasks::getNewTasksByDate();
 
         return $this->render('index', ['tasks' => $tasks]);
     }
