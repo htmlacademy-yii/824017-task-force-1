31142833d9b42e03d30c6f4d1a2b4a979cf96a3a
Михаил <serov.m@yandex.ru>
2021-04-06 21:19:31 +0300

    Удаляет ненужные комментарии. Переименовывает вид sign-up в index.

--- frontend/controllers/SignUpController.php
+++ frontend/controllers/SignUpController.php
@@ -5,85 +5,28 @@ declare(strict_types = 1);
 namespace frontend\controllers;
 
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use frontend\models\SearchUserForm;
-use frontend\models\Users;
+use frontend\models\user\Users;
 use yii\helpers\ArrayHelper;
-use frontend\models\Cities;
+use frontend\models\cities\Cities;
 use Yii;
 
 class SignUpController extends Controller
 {
     public function actionIndex()
     {
-        //var_dump(',,,,,,,,,');
         $cities = ArrayHelper::map(Cities::find()->asArray()->all(), 'id', 'name');
-        //var_dump('УРААА');
-
         $user = new Users;
-
         
         if (Yii::$app->request->getIsPost()) {
-            //var_dump('ДАА это ПОСТ');
             $user->load(Yii::$app->request->post());
 
             if ($user->validate()) {
-
                 $user->password = Yii::$app->security->generatePasswordHash($user->password);
                 $user->save(false);
-                
-                /*$this->goHome();*/
+                $this->goHome();
             }
         }
 
-        return $this->render('sign-up', ['model' => $user, 'cities' => $cities]);
-    } 
-
-    /*public function actionIndex()
-    {
-        $specializations = ArrayHelper::map(Specializations::find()->asArray()->all(), 'id', 'name');
-
-        $query = Users::find()->select(['users.*', 'AVG(rate) as rating', 'COUNT(rate) as finished_tasks_count', 'COUNT(comment) as comments_count'])->joinWith('specializations')->joinWith('reviews0')->joinWith('tasks0')->where(['role' => 'executant'])->groupBy('users.id')->orderBy(['signing_up_date' => SORT_DESC])->asArray();
-
-        $searchUserForm = new SearchUserForm;
-
-        $request = Yii::$app->request;
-
-        switch ($request->method) {
-            case 'GET':
-                $id = $request->get('specialization_id');
-
-                if (key_exists($id, $specializations)) {
-                    $searchUserForm->searchedSpecializations[$id] = $id;
-                    $query->andWhere(['specializations.id' => $searchUserForm->searchedSpecializations[$id]]);
-                }
-
-                break;
-
-            case 'POST':
-                if ($request->post('SearchUserForm')['searchedName']) {
-                    $searchUserForm->searchedName = $request->post('SearchUserForm')['searchedName'];
-                    $query->andWhere(['like', 'users.name', $searchUserForm->searchedName]);
-                } else {
-                    $searchUserForm->load($request->post());
-                    $query->andFilterWhere(['specializations.id' => $searchUserForm->searchedSpecializations]);
-                    $query->andFilterWhere(['>', 'favorite_count', $searchUserForm->isFavorite]);
-                    $query->andFilterHaving(['>', 'comments_count', $searchUserForm->hasReviews]);
-                
-                    if ($searchUserForm->isFreeNow) {
-                        $query->andWhere(['tasks.id' => null]);
-                    }
-
-                    if ($searchUserForm->isOnline) {
-                        $query->andWhere(['between', 'last_activity', strftime("%F %T", strtotime("-30 min")), strftime("%F %T")]);
-                    }
-                }
-
-                break;
-        }  
-
-        $users = $query->all();
-
-        return $this->render('index', ['users' => $users, 'searchUserForm' => $searchUserForm, 'specializations' => $specializations]);
-    }*/
+        return $this->render('index', ['model' => $user, 'cities' => $cities]);
+    }
 }
